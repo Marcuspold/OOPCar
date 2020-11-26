b2865e8bb2cc07189e5bdc58825b58a1c765c1e3
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:25:10 +0200

    first commit

---
+++ OOPCar/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Program
+    {
+        class Car
+        {
+            string markname;
+            string modelname;
+            string registration;
+            string color;
+            int odometer;
+            int fueltank;
+
+            public Car(string _markname, string _modelname, string _registration, string _color)
+            {
+                markname = _markname;
+                modelname = _modelname;
+                registration = _registration;
+                color = _color;
+                odometer = 0;
+                fueltank = 60;
+                Console.WriteLine($"The {modelname} of the car has been created");
+            }
+
+            public string Registration
+            {
+                get { return registration; }
+                set
+                {
+                    if (value.Length == 6)
+                    {
+                        Registration = value;
+                    }else
+                    {
+                        registration = "ERROR FAILURE";
+                    }
+                }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public void ShowCarData()
+            {
+                Console.WriteLine($"markname: {markname}; modelname: {modelname}; registration {registration}; color {color}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Car newCar = new Car("Audi", "T8", "TV343", "yellow" );
+            Console.WriteLine(newCar.Color);
+            Console.WriteLine(newCar.Registration);
+
+
+        }
+    }
+}
---
+++ OOPChallenge/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OOPChallenge
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The Dog {name} has been created.");
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Woof-Woof");
+                happiness += 0.2;
+            }
+            public void Wiggle()
+            {
+                Console.WriteLine("Wiggle-Wiggle");
+
+            }
+            public void Rename(string newName) //rename
+            {
+                name = newName;
+            }
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happy {happiness};");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            Dog newDog = new Dog("Poggy", 10);
+            
+            Console.WriteLine($"The Level Of Happiness: {newDog.Happiness}");
+
+            while(newDog.Happiness != 1)
+            {
+                newDog.Barks();
+            }
+            newDog.Wiggle();
+
+            Console.WriteLine("Name the Dog:");
+            string newDogName = Console.ReadLine();
+            newDog.Rename(newDogName);
+            newDog.ShowDogData();
+
+
+        }
+    }
+}
